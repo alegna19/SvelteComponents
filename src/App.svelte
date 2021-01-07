67c0bb9f0ee03cf0ec4f1f9e5b6c367b6f7c6e82
Angela Gomez <angela.gomez@Angie.local>
2021-01-07 15:50:39 +0000

    components

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	import Card from './Posts/Card.svelte';
+	import Header from './Design/Header.svelte';
+	const color = "warning"
+	const post = [
+		{
+			id:'1',
+			title: 'Working with Svelte',
+			description:'Svelte In the Morning',
+			imagen: 'https://cdn.pixabay.com/photo/2020/12/29/10/07/coast-5870088_1280.jpg'
+		},
+		{
+			id:'2',
+			title: 'Working with Svelte',
+			description:'Svelte In the Affternoon',
+			imagen: 'https://cdn.pixabay.com/photo/2021/01/01/14/07/chapel-5878656_1280.jpg'
+		},
+		{
+			id:'3',
+			title: 'Working with Svelte',
+			description:'Svelte at nigth',
+			imagen: 'https://cdn.pixabay.com/photo/2020/12/28/18/32/mountains-5868127_1280.jpg'
+		}
+	]
+</script>
+
+<Header title="Components" {color}/>
+<Header title="Mi Second Navbar" color="info"/>
+
+<div class="container">
+	{#each post as item}
+		<Card>
+			title = {item.title}
+			description = {item.description}
+			imagen = {item.imagen}
+		</Card>
+
+	{/each}
+</div>
---
+++ src/Posts/Card.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let title
+    export let description
+    export let imagen
+</script>
+
+<div class="card" style="width: 18res;">
+    <img src={imagen} alt={title} width="100%" height="200">
+    <div class="card-body">
+        <h5 class="card-title">{title}</h5>
+        <p class="card-text">{description}</p>
+        <button class="btn btn-primary">More...</button>
+        <button class="btn btn-danger">Favorite </button>
+    </div>
+</div>
